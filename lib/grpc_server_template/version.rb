c180d906114a7882979a379adff6c6340ab205dc
Sylvance <kerandisylvance@gmail.com>
2023-01-11 00:51:16 +0300

    [grpc_server_template] Bundle initial gem files

---
+++ lib/grpc_server_template/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GrpcServerTemplate
+  VERSION = "0.1.0"
+end
---
+++ spec/grpc_server_template_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe GrpcServerTemplate do
+  it "has a version number" do
+    expect(GrpcServerTemplate::VERSION).not_to be nil
+  end
+end
